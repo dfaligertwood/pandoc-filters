aab2cbeedb3a5a6a1e15a84219330d4a7b0c0b7b
Douglas Ligertwood <dfaligertwood@dunelm.org.uk>
2014-12-04 15:48:33 +0000

    Various stuff to do with pandoc-filters.rb.
    Not really of any use to anyone: brief flirtation with the idea of porting the pandocfilters.py framework to ruby.

--- pandoc-filters.rb
+++ pandoc-filters.rb
@@ -1,6 +1,6 @@
 # Author: D F A Ligertwood <dfaligertwood@dunelm.org.uk>
 # License: BSD3
-
+#
 # Port of pandocfilters.py by John MacFarlane, available at
 # github.com/jgm/pandocfilters
 #
@@ -9,238 +9,205 @@
 
 require 'json'
 
-class Object
-	def to_pandoc(meta)
-		self
-	end
+module Pandoc
+    def walk(&action)
+        pandoc_map { |item| item.walk &action }
+    end
+    def owalk(&action)
+        pandoc_map { |item| item.owalk &action }
+    end
+end
 
-	def walk(action)
-		self
-	end
+class Object
+    def pandoc_map(*a) self end
+    include PandocMethods
 end
 
 class Hash
-	def to_pandoc(meta)
-		if self['t']
-			Pandoc.new(self['t'], self['c'].to_pandoc(meta), meta)
-		else
-			a = self
-			a.each { |k,v|
-				a[k] = v.to_pandoc(meta)
-			}
-		end
-	end
+    def pandoc_map(&block)
+        map{ |k,v| [k, block.(v)] }.to_h
+    end
+    include PandocMethods
+
+    def to_pandoc(meta)
+        if self['t']
+            PandocItem.new(self['t'], self['c'].to_pandoc(meta), meta)
+        else
+            super
+        end
+    end
 end
 
 class Array
-	def to_pandoc(meta)
-		self.map { |item|
-			item.to_pandoc(meta)
-		}
-	end
-
-	def walk(action)
-		self.map { |item|
-			item.walk(action)
-		}
-	end
+    def pandoc_map(&block)
+        a = Array.new
+        each { |item|
+            result = block.(item)
+            if result.is_a? Array and not item.is_a? Array
+                a += result
+            else
+                a << result
+            end
+        }
+        a
+    end
+    include PandocMethods
 end
 
-class Pandoc
-	attr :type, :contents, :meta
-	def initialize(type, contents, meta)
-		@format = if ARGV.length > 1
-				  ARGV[1]
-			  else
-				  ""
-			  end
-		@type = type
-		@contents = contents
-		@meta = meta
-	end
+class PandocItem < Hash
+    attr_reader :format, :meta
+    def type; self['t']; end
+    def type=(a); self['t']=a; end
+    def contents; self['c']; end
+    def contents=(a); self['c']=a; end
 
-	def self.new_document
-		document = JSON.load(STDIN)
-		meta = document[0]["unMeta"]
-		contents = document[1].to_pandoc(meta)
-		type = "Document" 
-		Pandoc.new(type, contents, meta)
-	end
+    def initialize type, contents, meta, *a
+        @format = ARGV[1] || String.new
+        @meta, self.type, self.contents = meta, type, contents
+        super *a
+    end
 
-	def walk(action)
-		result = self.send action
-		if result
-			return result.walk(action)
-		else
-			return @contents.walk(action)
-		end
-	end
-	
-	def to_hash
-		hash = Hash.new
-		hash['t'] = self.type
-		hash['c'] = self.contents
-		hash
-	end
-	alias to_h to_hash
+    def walk(&action)
+        result = action.call self.type, self.contents, self.format, self.meta
+        result.walk &action or PandocItem.new(self.type, self.contents.walk(&action), self.meta)
+    end
 
-	def to_json(*args)
-		if @type.to_s == "Document"
-			document = Array.new
-			document[0] = Hash.new
-			document[0]["unMeta"] = @meta
-			document[1] = @contents
-			return JSON.dump(document)
-		else
-			return JSON.dump(self.to_hash)
-		end
-	end
+    def owalk(&action)
+        result = action.call self
+        result.owalk &action or self.contents.owalk &action
+    end
 
-	def self.toJSONFilter(action)
-		document = Pandoc.new_document
-		document.walk(action)
-		$stdout.print document.to_json
-	end
+    def attributes
+        if self.contents[0].is_a? Array
+            return {id: self.contents[0][0],
+                    classes: self.contents[0][1]}.merge(self.contents[0][2].to_h)
+        else
+            return false
+        end
+    end
+
+    def attributes=(attrs)
+        begin
+            attrs.update(self.attributes) { |k,o,n| o }
+            keys = attrs.reject{ |k| k == "id" or k == "classes" }.to_a
+            self.contents[0] = [attrs['id'], attrs['classes'], keys]
+        rescue
+            if attrs.is_a? Hash
+                $stderr.puts "#{self.type} does not accept attributes."
+            else
+                $stderr.puts "Incorrect format for attributes."
+            end
+        end
+    end
+
+    # Stringify function. Example of how to use 'walk'. Note 'nil'
+    # at end of function. This is necessary because in ruby nearly
+    # everything returns a value, and we don't want to affect the
+    # AST.
+
+    def stringify
+        result = String.new
+        walk { |type, contents|
+            case type
+            when "Str"
+                result << contents.to_s
+            when "Code", "Math"
+                result << contents[1].to_s
+            when "LineBreak", "Space"
+                result << " "
+            end
+            nil
+        }
+        result
+    end
+
+    # Constructors.
+
+    class <<self
+        block_elements = {
+            HorizontalRule: 0,
+            Null: 0,
+            BlockQuote: 1,
+            BulletList: 1,
+            DefinitionList: 1,
+            Para: 1,
+            Plain: 1,
+            CodeBlock: 2,
+            Div: 2,
+            OrderedList: 2,
+            RawBlock: 2,
+            Header: 3,
+            Table: 5
+        }
+
+        inline_elements = {
+            LineBreak: 0,
+            Space: 0,
+            Emph: 1,
+            Note: 1,
+            SmallCaps: 1,
+            Str: 1,
+            Strong: 1,
+            Strikeout: 1,
+            Subscript: 1,
+            Superscript: 1,
+            Cite: 2,
+            Code: 2,
+            Image: 2,
+            Link: 2,
+            Math: 2,
+            Quoted: 2,
+            RawInline: 2,
+            Span: 2
+        }
+
+        inline_elements.merge(block_elements).each { |eltType, numargs|
+            define_method(eltType) { |*args|
+                raise ArgumentError, "#{eltType} expects #{numargs} arguments, \
+                      but received #{args.length} arguments" unless numargs == args.length
+
+                if numargs == 1
+                    return PandocItem.new(eltType.to_s, args[0], {})
+                else
+                    return PandocItem.new(eltType.to_s, args, {})
+                end
+            }
+        }
+    end
 end
 
-# Stringify function. Example of how to define actions.
+class Pandoc < PandocItem
+    def initialize document, *a
+        meta = document[0]["unMeta"]
+        contents = document[1].to_pandoc(meta)
+        super "Document", contents, meta, *a
+    end
+    def to_json
+        return [{"unMeta" => self.meta}, self.contents].to_json; 
+    end
+    def pandoc_map(&block)
+        Pandoc.new([{"unmeta" => self.meta}, block.(self.contents)])
+    end
+    def walk(&action)
+        pandoc_map { |item| item.walk &action }
+    end
+    def owalk(&action)
+        pandoc_map { |item| item.owalk &action }
+    end
 
-class Pandoc
-	@@stringify_result = String.new
-
-	def stringify
-		@@stringify_result = ""
-		self.walk :stringify_action
-		return @@stringify_result
-	end
-	alias to_s stringify
-
-	def stringify_action
-		case @type
-		when "Str"
-			@@stringify_result << @contents.to_s
-		when "Code", "Math"
-			@@stringify_result << @contents[1].to_s
-		when "LineBreak", "Space"
-			@@stringify_result << " "
-		end
-		false
-	end
-end
-
-# Constructors.
-
-class Pandoc
-	private
-	def self.elt(eltType, numargs, *args)
-		raise ArgumentError, "#{eltType} expects #{numargs} arguments, \
-			but received #{args.length} arguments" unless numargs == args.length
-		if numargs == 1
-			return Pandoc.new(eltType, args[0], {})
-		else
-			return Pandoc.new(eltType, args, {})
-		end
-	end
-
-	public
-	class <<self
-		# Block Elements
-
-		def Plain(*args)
-			elt('Plain', 1, *args)
-		end
-		def Para(*args)
-			elt('Para', 1, *args)
-		end
-		def CodeBlock(*args)
-			elt('CodeBlock', 2, *args)
-		end
-		def RawBlock(*args)
-			elt('RawBlock', 2, *args)
-		end
-		def BlockQuote(*args)
-			elt('BlockQuote', 1, *args)
-		end
-		def OrderedList(*args)
-			elt('OrderedList', 2, *args)
-		end
-		def BulletList(*args)
-			elt('BulletList', 1, *args)
-		end
-		def DefinitionList(*args)
-			elt('DefinitionList', 1, *args)
-		end
-		def Header(*args)
-			elt('Header', 3, *args)
-		end
-		def HorizontalRule(*args)
-			elt('HorizontalRule', 0, *args)
-		end
-		def Table(*args)
-			elt('Table', 5, *args)
-		end
-		def Div(*args)
-			elt('Div', 2, *args)
-		end
-		def Null(*args)
-			elt('Null', 0, *args)
-		end
-
-		# Inline Elements
-
-		def Str(*args)
-			elt('Str', 1, *args)
-		end
-		def Emph(*args)
-			elt('Emph', 1, *args)
-		end
-		def Strong(*args)
-			elt('Strong', 1, *args)
-		end
-		def Strikeout(*args)
-			elt('Strikeout', 1, *args)
-		end
-		def Superscript(*args)
-			elt('Superscript', 1, *args)
-		end
-		def Subscript(*args)
-			elt('Subscript', 1, *args)
-		end
-		def SmallCaps(*args)
-			elt('SmallCaps', 1, *args)
-		end
-		def Quoted(*args)
-			elt('Quoted', 2, *args)
-		end
-		def Cite(*args)
-			elt('Cite', 2, *args)
-		end
-		def Code(*args)
-			elt('Code', 2, *args)
-		end
-		def Space(*args)
-			elt('Space', 0, *args)
-		end
-		def LineBreak(*args)
-			elt('LineBreak', 0, *args)
-		end
-		def Math(*args)
-			elt('Math', 2, *args)
-		end
-		def RawInline(*args)
-			elt('RawInline', 2, *args)
-		end
-		def Link(*args)
-			elt('Link', 2, *args)
-		end
-		def Image(*args)
-			elt('Image', 2, *args)
-		end
-		def Note(*args)
-			elt('Note', 1, *args)
-		end
-		def Span(*args)
-			elt('Span', 2, *args)
-		end
-	end
+    class <<self
+        def from_stdin; new(JSON.load(STDIN)); end
+        def toJSONFilter(&action)
+            document = Pandoc.new(JSON.load(STDIN))
+            puts document.class
+            document = document.walk &action
+            puts document.class
+            $stdout.print document.to_json
+        end
+        def filter(&action)
+            document = from_stdin
+            document = document.owalk &action
+            $stdout.print document.to_json
+        end
+    end
 end
