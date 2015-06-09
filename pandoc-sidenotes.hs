3a17e119a20c7adb8c61a6f1f7d4faaa5ca62052
Douglas Ligertwood <dfaligertwood@dunelm.org.uk>
2015-06-09 22:43:31 +0100

    Fix pandoc-sidenotes.

--- pandoc-sidenotes.hs
+++ pandoc-sidenotes.hs
@@ -3,21 +3,11 @@
 --------------------------------------------------------------------------------
 
 import           Text.Pandoc.JSON
-import           Text.Pandoc.Generic
-import qualified Data.Map as M
-import           Data.Map
-  ( Map )
 import           Utils
 
 --------------------------------------------------------------------------------
 -- This filter converts all footnotes with the relevant initial letter into
 -- sidenotes using the marginnote.sty package for LaTeX.
-
-sidenoteKeys :: Map String Inline
-sidenoteKeys = M.fromList
-              [ (">", latexInline "\\normalmarginpar\\marginnote{")
-              , ("~", latexInline "\\reversemarginpar\\marginnote{")
-              ]
 
 main :: IO ()
 main = toJSONFilter sidenotes
@@ -27,20 +17,13 @@ sidenotes (Just "latex")
     = unionMetaInline
         "header-includes"
         (RawInline "tex" "\\usepackage{marginnote}")
-    . bottomUp (concatMap addSidenotes)
-sidenotes _ = id
-
-addSidenotes :: Inline -> [Inline]
-addSidenotes (Note contents)
-  = if M.member firstLetter sidenoteKeys
-      then concat
-           [ [ sidenoteKeys M.! firstLetter ]
-           , concatMap toInline contents
-           , [ latexInline "}" ]
-           ]
-      else [Note contents]
+    . note ">"
+           (latexInline "\\normalmarginpar\\marginnote{")
+    . note "~"
+           (latexInline "\\reversemarginpar\\marginnote{")
   where
-    firstLetter = take 1 $ show contents
-addSidenotes x = [x]
+    note a b = overloadNote a b (latexInline "}") id
+sidenotes _ = rmStr ">"
+            . rmStr "~"
 
 --------------------------------------------------------------------------------
